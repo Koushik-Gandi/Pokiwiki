5038a9a8b4b2f53f3464a5718771e4f9ce7881db
Mrudhukoushik_Gandi <47925723+Koushik-Gandi@users.noreply.github.com>
2025-05-22 09:59:35 +0300

    updates

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Pokemon } from '../models/pokemon.model';
 import { PokedexService } from '../services/pokedex.service';
 
@@ -10,6 +10,10 @@ import { PokedexService } from '../services/pokedex.service';
 })
 export class PokedexComponent implements OnInit {
   pokemons: Pokemon[] = [];
+  gridClass: string = 'three-columns';
+  generations: string[] = ['Gen1', 'Gen2', 'Gen3', 'Gen4'];
+  selectedGeneration: string = 'Gen1';
+  showScrollToTop: boolean = false;
 
   constructor(private pokedexService: PokedexService) { }
 
@@ -18,16 +22,20 @@ export class PokedexComponent implements OnInit {
   }
 
   fetchPokemonData(): void {
-
-    // Fetch Pokémon data from the local db.json file
+    // Fetch Pokémon data from the local JSON file
     this.pokedexService.fetchLocalPokemonList().subscribe({
-      next: (data) => {
-        this.pokemons = data.PokemonList.Generations[0].Generation1;
+      next: (data) => {        
+        const generationsData = data.PokemonList.Generations;
+        const generationIndex = this.generations.indexOf(this.selectedGeneration);
+        if (generationIndex !== -1) {
+          this.pokemons = generationsData[this.selectedGeneration];          
+        }
       },
       error: (error) => {
         console.error('Error fetching Pokémon data:', error);
       }
     });
+
 
     // Uncomment the following lines to fetch data from the API instead of the local file
 
@@ -39,9 +47,30 @@ export class PokedexComponent implements OnInit {
     //     console.error('Error fetching Pokémon data:', error);
     //   }
     // });
+
   }
 
   formatIndex(index: number): string {
     return `#${index.toString().padStart(4, '0')}`;
+  }
+
+  updateGridColumns(event: Event): void {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.gridClass = selectedValue === '3' ? 'three-columns' : 'four-columns';
+  }
+
+  switchGeneration(generation: string): void {
+    this.selectedGeneration = generation;
+    this.fetchPokemonData();
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      this.showScrollToTop = scrollPosition > 1000;
   }
 }
